9fa21e15f644150ddb62a89ec358eccc311d05ad
Celina <idkmahee@gmail.com>
2025-09-19 01:16:29 -0300

    se cambiaron los estilos del nav

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,6 +1,7 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { useNavigate, useLocation } from "react-router";
-
+import { useNavigate, useLocation, Link } from "react-router";
+import logo from "../../assets/logo-ossa.png";
+import "../styles/menu.css";
 function Menu() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -19,13 +20,23 @@ function Menu() {
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Navbar bg="light" variant="light" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="/">OSSA</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            alt="logo Ossa ing"
+            className="img-fluid me-2"
+            width={50}
+          />
+          OSSA Ingeniería y Construcción S.R.L
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Inicio
+            </Nav.Link>
             <Nav.Link onClick={() => handleScroll("servicios")}>
               Servicios
             </Nav.Link>
